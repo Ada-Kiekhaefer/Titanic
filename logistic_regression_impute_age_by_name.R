27834db453883817e324d8cbce7a91c63781dfd7
Ada-Kiekhaefer <ada.kiekhaefer@gmail.com>
2020-02-04 11:34:28 -0600

    reorganize files

--- logistic_regression_impute_age_by_name.R
+++ logistic_regression_impute_age_by_name.R
@@ -13,12 +13,37 @@ library(caTools)
 library(na.tools)
 library(tidyimpute)
 
-#training data set
-train_all_column <- read_csv("train.csv")
-train <- train_all_column %>% select(Survived, Sex, Age, Pclass, Embarked) %>%
-  impute_at( .na=na.mean, .vars = 'Age') %>% #impute missing age with mean values
+#read training data set
+train_all_column <- read_csv("train.csv") %>%
+  mutate(Survived=factor(Survived), Sex=factor(Sex), 
+         Pclass=factor(Pclass), Embarked=factor(Embarked)) %>%
+  arrange(PassengerId)
+
+#Calculate average age of passenger who use Master as title (Boys)
+master_mean <- train_all_column %>%
+  select(Name,Age) %>%
+  filter(grepl('Master', Name)) %>%
   drop_na() %>%
-  mutate(Survived=factor(Survived), Sex=factor(Sex), Pclass=factor(Pclass), Embarked=factor(Embarked))
+  summarize(mean = mean(Age))
+
+train_all_column_imputed <- train_all_column %>%
+  mutate(age_imputed = Age) %>%
+  mutate(master_bool = grepl('Master', Name))
+master_ind <- which(train_all_column_imputed$master_bool == TRUE)
+master_na_ind <- which(train_all_column_imputed$master_bool & is.na(train_all_column_imputed$Age))
+train_all_column_imputed[master_na_ind,'age_imputed'] <- master_mean$mean
+
+miss_child_mean <- train_all_column %>%
+  select(Name, Age, Parch) %>%
+  filter(grepl('Miss', Name)) %>%
+  filter(Parch >0) %>%
+  drop_na() %>%
+  summarize(mean = mean(Age))
+train_all_column_imputed <- train_all_column_imputed %>%
+  mutate(miss_child_bool = grepl('Miss', Name) & Parch >0)
+miss_child_ind <- which(train_all_column_imputed$miss_child_bool == TRUE)
+miss_child_na_ind <- which(train_all_column_imputed$miss_child_bool & is.na(train_all_column_imputed$Age))
+train_all_column_imputed[miss_child_na_ind,'age_imputed'] <- miss_child_mean$mean
 
 #Split training and testing data
 set.seed(42)
@@ -40,6 +65,8 @@ confusion_matrix_gender <- table(test_actual, test_predicted_gender)
 model_performance_gender <- tibble(accuracy = accuracy(test_actual, test_predicted_gender),
                                    precision = precision(test_actual, test_predicted_gender),
                                    recall = recall(test_actual, test_predicted_gender))
+
+
 
 #creat ROC curve
 p_gender <- predict(model_gender, testing_data, type = "response")
